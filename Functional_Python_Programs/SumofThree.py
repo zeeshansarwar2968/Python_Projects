0438b99e0754964cfd64201b05daeb35b5249c8b
Zeeshan Sarwar <zeeshansarwar2968@gmail.com>
2022-06-25 09:23:43 +0530

    [Zeeshan] : Updated readme file and added comments

--- Functional_Python_Programs/SumofThree.py
+++ Functional_Python_Programs/SumofThree.py
@@ -16,12 +16,11 @@ def find_triplets(n, arr):
     """
     found = False
     for i in range(0, n - 2):  # Looping in to the array elements till (n-2) element for the 'i' th element
-        for j in range(i + 1, n - 1):  # Looping in to the array from the (i+1)th elements position till  penultimate
-            # element
-            for k in range(j + 1, n):  # Taking the jth elements position till the last elements position
-                if (arr[i] + arr[j] + arr[k]) == 0:  # Checking the sum of ith jth and kth  to be Zero
+        for j in range(i + 1, n - 1):  # Looping in to the array from the (i+1)th elements position till last element
+            for k in range(j + 1, n):  # Taking the jth elements position till the last position
+                if (arr[i] + arr[j] + arr[k]) == 0:  # Checking the sum of ith, jth and kth to be Zero
                     found = True
-                    return arr[i], arr[j], arr[k]  # Returning the elements If a+b+c = 0
+                    return arr[i], arr[j], arr[k]  # Returning the elements if a+b+c = 0
     if not found:
         return "Does not exist"
 
